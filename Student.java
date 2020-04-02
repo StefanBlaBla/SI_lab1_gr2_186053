5d985838ea113377d5cd0474c6d0d4e263e1fbb7
StefanDimeski <stefandimeski90@gmail.com>
2020-04-03 01:01:10 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -4,15 +4,48 @@ class Student {
 	String lastName;
 	List<Integer> grades;
 
-	//TODO constructor
+	Student(String id, String fn, String ln, List<Integer> grades)
+	{
+		this.index = id;
+		this.firstName = fn;
+		this.lastName = ln;
+		this.grades = grades;
+	}
 
-	//TODO seters & getters
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
 	public double getAverage() {
-		//TODO
+		return grades.stream()
+				.mapToInt(x -> x)
+				.average().getAsDouble();
 	}
 
 	public int ECTSCredits() {
-		//TODO
+		return grades.stream()
+				.filter(x -> x > 5)
+				.reduce((summary, current) -> summary + 6)
+				.get();
 	}
 }
